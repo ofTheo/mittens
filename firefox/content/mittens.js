ded885e51fbd59e38f9dce5ce96c71fae0152da4
David Cook <divergentdave@gmail.com>
2012-02-22 20:38:43 -0600

    Rewrite URL using HTML5 History API

--- firefox/content/mittens.js
+++ firefox/content/mittens.js
@@ -3,6 +3,8 @@
 var xpath = "descendant-or-self::text()[contains(., 'mitt ') or contains(., 'Mitt ')]";
 var regex = /(M)itt /ig;
 var replacement = '$1ittens ';
+var regexURL = /(M)itt([^a-z])/ig;
+var replacementURL = '$1ittens$2';
 
 function traverseSubtree(base) {
 	var list = document.evaluate(xpath, base, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -22,6 +24,10 @@ document.body.addEventListener('DOMCharacterDataModified', function(event) {
 		event.target.nodeValue = event.newValue.replace(regex, replacement);
 	}
 }, false);
+
+if (location.href.match(regexURL)) {
+	history.replaceState(null, '', location.pathname.replace(regexURL, replacementURL) + location.hash.replace(regexURL, replacementURL));
+}
 
 })();
 
